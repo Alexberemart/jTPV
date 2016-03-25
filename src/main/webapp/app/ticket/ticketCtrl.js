9dc931015cd1071c86c7137dd06d2bf4ac9a7538
Alejandro Berenguer <alexberemart@gmail.com>
2016-03-25 16:32:31 +0100

    create ticket controller

---
+++ src/main/webapp/app/ticket/ticketCtrl.js
@@ -0,0 +1,23 @@
+angular.module('app.Controllers')
+    .controller('ticketCtrl',
+        function ($scope, $uibModalInstance, transaction, urlConstantsFact, $http, $rootScope) {
+            $scope.transaction = transaction;
+            $scope.currentDate = new Date();
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+            $scope.printToCart = function () {
+                html2canvas(document.getElementById("ticketPanel"), {
+                    onrendered: function (canvas) {
+                        var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+                        popupWinindow.document.open();
+                        popupWinindow.document.write('<html><head></head><body onload="window.print()"><img src="' + canvas.toDataURL() + '"></html>');
+                        popupWinindow.document.close();
+                    }
+                });
+            };
+            $scope.compareNotEquals = function (actual, expected) {
+                return actual !== expected;
+            };
+        }
+    );
